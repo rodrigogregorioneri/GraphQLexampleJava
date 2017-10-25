811cdea107cd79797a9df11d7a47e3d402a45327
rodrigogregorioneri <rodrigogregorioneri>
2017-10-25 13:09:54 +0000

    criando metodo de Listagem de Usuarios

---
+++ src/main/java/com/example/graphql/business/UserBusiness.java
@@ -0,0 +1,37 @@
+package com.example.graphql.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.example.graphql.dao.UserDAO;
+import com.example.graphql.entity.User;
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Rodrigo Gregorio Neri
+ * @version 1.0
+ * @since 2017-10-25
+ *
+ */
+ 
+@Service("UserBusiness")
+public class UserBusiness {
+
+
+  @Autowired
+  @Qualifier("UserDAO")
+  protected UserDAO repository;
+
+
+
+  public List<User> list(){
+    List<User> result = repository.list();
+    return result;
+  }
+
+
+}
